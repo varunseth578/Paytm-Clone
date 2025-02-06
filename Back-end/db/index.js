7d5ed34655d58b631037995f5d374e22fe61e6c8
varunseth578 <ivarunseth@gmail.com>
2025-02-06 20:55:53 +0530

    create routes

--- Back-end/db/index.js
+++ Back-end/db/index.js
@@ -1,13 +1,10 @@
 const mongoose = require("mongoose");
 
-// Connect to MongoDB
 mongoose.connect(
   "mongodb+srv://ivarunseth:yLMkrysxa42X6ORu@cluster0.sea1c.mongodb.net/"
 );
 
-// Define schemas
 const userSchema = mongoose.Schema({
-  // Schema definition here
   username: String,
   password: String,
   firstName: String,
--- Back-end/index.js
+++ Back-end/index.js
@@ -1,22 +1,13 @@
-const express = require('express');
-const cors = require("cors");
-
-app.use(cors());
-app.use(express.json());
+const express = require("express");
 
 const mainRouter = require("./routes/index");
 
-const app = express();
-
-
-
-app.use("/api/v1",mainRouter)
+app.use("/api/v1", mainRouter);
 
 // / api/v1/user/signup
 // / api/v1/user/sigin
 // / api/v1/user/changepassword
 const PORT = 3000;
-app.listen(PORT,()=>{
-    console.log(`server is running on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`server is running on port ${PORT}`);
 });
-
