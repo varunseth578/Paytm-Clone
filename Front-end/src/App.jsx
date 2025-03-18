895b8b044318e8e2457658e9e6fd3be58ce955f8
varunseth578 <ivarunseth@gmail.com>
2025-03-18 12:10:16 +0530

    add account

--- Front-end/src/App.jsx
+++ Front-end/src/App.jsx
@@ -1,25 +1,28 @@
-import React from 'react';
-import { BrowserRouter, Routes,Route } from 'react-router-dom';
-import "./App.css";
-import { Signup } from './pages/Signup';
-import { Signin } from './pages/Signin';
-import { Dashboard } from './pages/Dashboard';
-import { SendMoney } from './pages/Send';
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Suspense, lazy } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+const Signup = lazy(() => import('./pages/Signup'));
+const Signin = lazy(() => import('./pages/Signin'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const SendMoney = lazy(() => import('./pages/SendMoney'));
+const UpdateDetails = lazy(() => import('./pages/UpdateDetails'));
 function App() {
-  return (
-   <>
-   <BrowserRouter>
-      <Routes>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/signin" element={<Signin />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/send" element={<SendMoney/>} />
 
-      </Routes>
-    </BrowserRouter>
+  return (
+    <>
+      <BrowserRouter>
+      <ToastContainer />
+        <Routes>
+          <Route path="/" element={<Suspense fallback={"loading..."}><Signup /></Suspense>} />
+          <Route path="/signin" element={<Suspense fallback={"loading..."}><Signin /></Suspense>} />
+          <Route path="/dashboard" element={<Suspense fallback={"loading..."}><Dashboard /></Suspense>} />
+          <Route path="/send" element={<Suspense fallback={"loading..."}><SendMoney /></Suspense>} />
+          <Route path="/updateDetails" element={<Suspense fallback={"loading..."}><UpdateDetails /></Suspense>} />
+        </Routes>
+      </BrowserRouter>  
     </>
-  );
+  )
 }
 
-export default App;
+export default App
